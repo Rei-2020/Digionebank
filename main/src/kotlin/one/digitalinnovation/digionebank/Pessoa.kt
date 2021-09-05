37ccc5f067639964e756920e767f694f0298b257
Rei-2020 <renildo.b.alves@gmail.com>
2021-09-04 23:34:10 -0300

    criei classe Pessoa.kt e fun main para teste

---
+++ main/src/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    val nome: String ="Renildo Alves"
+    val cpf: String ="098.452.488-61"
+
+}
+fun main(){
+ val Renildo = Pessoa()
+    println(Renildo.nome)
+    println(Renildo.cpf)
+
+}
