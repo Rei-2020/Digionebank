b8c33260dc5987934e278cab03a100c6a2393a2b
Rei-2020 <renildo.b.alves@gmail.com>
2021-09-05 10:24:25 -0300

    alteração geral

--- main/src/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ main/src/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,13 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    val nome: String ="Renildo Alves"
-    val cpf: String ="094.452.448-33"
+    var nome: String ="Lara"
+    var cpf: String ="094.452.448-22"
+    private set
 
+    constructor()
+    fun pessoainfo() = "$nome e $cpf"
 }
 fun main(){
- val Renildo = Pessoa()
-    println(Renildo.nome)
-    println(Renildo.cpf)
+ val Lara = Pessoa()
+    println(Lara. pessoainfo())
 
 }
