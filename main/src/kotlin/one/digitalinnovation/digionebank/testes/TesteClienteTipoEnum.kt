cac1f115dfe6c959efead005021221a5e045f8cd
Rei-2020 <renildo.b.alves@gmail.com>
2021-09-06 08:36:58 -0300

    teste cliente tipo

--- main/src/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
+++ main/src/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
@@ -10,4 +10,10 @@ fun main() {
     ClienteTipo.values().forEach {
         println("${it.name} - ${it.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println(">>${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">>${pj.name} - ${pj.descricao}")
 }
