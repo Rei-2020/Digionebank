b9f4dec011371e832e2f0c606e2104fdd29cd197
Rei-2020 <renildo.b.alves@gmail.com>
2021-09-12 10:32:08 -0300

    Final atualização

--- main/src/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ main/src/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,7 +4,10 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main(){
 
-    val jose = Gerente("Jose Silva", "12.300.234-66",5000.0)
+    val jose = Gerente("Jose Silva", "12.300.234-66",5000.0, senha = "123")
+
     ImprimeRelatorioFuncionario.imprime(jose)
+
+    TesteAutenticacao().autentica(jose)
 }
 
